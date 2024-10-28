5ce9487a3af211102e3630f2cc5e95853e38b343
EduardoTakeiYaginuma <eduyagnuma10@gmail.com>
2024-10-28 10:47:04 -0300

    starting project

---
+++ grupo2/src/test/java/insper/br/grupo2/Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package insper.br.grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
