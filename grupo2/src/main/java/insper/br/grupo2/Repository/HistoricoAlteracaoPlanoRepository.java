42e3bacc3f4c46d25f0016d4159d557ee62a2f9e
mateus <mateusmoreirammp052@gmail.com>
2024-11-04 11:18:58 -0300

    agora vai

--- grupo2/src/main/java/insper/br/grupo2/Repository/HistoricoAlteracaoPlanoRepository.java
+++ grupo2/src/main/java/insper/br/grupo2/Repository/HistoricoAlteracaoPlanoRepository.java
@@ -4,6 +4,5 @@ import insper.br.grupo2.Classes.HistoricoAlteracaoPlano;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface HistoricoAlteracaoPlanoRepository extends MongoRepository<HistoricoAlteracaoPlano, String> {
-    HistoricoAlteracaoPlano findByUsuarioId(String usuarioId);
     HistoricoAlteracaoPlano findByEmail(String email);
 }
